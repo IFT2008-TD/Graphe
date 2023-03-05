95c6f17e5c8e6a49a7b6d164b3d604706aa3d228
Pascal Charpentier <charpentier_pasc@hotmail.com>
2023-03-05 17:43:15 -0500

    Doc dans graphe_algorithmes.h

--- graphes_algorithmes.h
+++ graphes_algorithmes.h
@@ -1,6 +1,9 @@
-//
-// Created by Pascal Charpentier on 2023-02-22.
-//
+/**
+ * @file graphes_algorithmes.h
+ * @author Pascal Charpentier
+ * @date 2023-03-04
+ * @version 0.1
+ */
 
 #ifndef GRAPHE_BASE_GRAPHES_ALGORITHMES_H
 #define GRAPHE_BASE_GRAPHES_ALGORITHMES_H
@@ -9,15 +12,28 @@
 #include <stack>
 #include <queue>
 
+/**
+ * Type servant à passer les informations nécessaires à la fonctions récursive aux_explorer_DFS: celle-ci doit avoir
+ * une copie du graphe, la pile des noeuds abandonnés et l'ensemble des noeuds parcourus.
+ * @tparam T
+ * @tparam P
+ */
 template <typename T, typename P>
 struct ResultatsDFS_t {
-    Graphe<T, P> graphe ;
-    std::stack<T> accumulateur ;
-    std::unordered_set<T> parcourus ;
+    Graphe<T, P>          graphe ;       //!< Le graphe à visiter
+    std::stack<T>         accumulateur ; //!< Pile des noeuds abandonnés
+    std::unordered_set<T> parcourus ;    //!< Ensemble des noeuds parcourus
 
     explicit ResultatsDFS_t(Graphe<T, P> graphe) : graphe(graphe) {}
 };
 
+/**
+ * Exploration en profondeur à partir d'un noeud de départ donné.
+ * @tparam T
+ * @tparam P
+ * @param dfsInfo struct contenant le graphe, la pile des noeuds abandonnés et les noeuds parcourus.
+ * @param sommet Sommet de départ.
+ */
 template <typename T, typename P>
 void aux_explorer_DFS(ResultatsDFS_t<T, P>& dfsInfo, const T& sommet) {
     dfsInfo.parcourus.insert(sommet) ;
@@ -26,6 +42,13 @@ void aux_explorer_DFS(ResultatsDFS_t<T, P>& dfsInfo, const T& sommet) {
     dfsInfo.accumulateur.push(sommet) ;
 }
 
+/**
+ * Exploration en profondeur complète d'un graphe.
+ * @tparam T
+ * @tparam P
+ * @param graphe L'objet graphe à explorer.
+ * @return La pile des noeuds abandonnés.
+ */
 template <typename T, typename P>
 std::stack<T> explorer_DFS(const Graphe<T, P>& graphe) {
     struct ResultatsDFS_t<T, P> dfsInfo(graphe) ;
@@ -36,6 +59,13 @@ std::stack<T> explorer_DFS(const Graphe<T, P>& graphe) {
     return dfsInfo.accumulateur ;
 }
 
+/**
+ * Construit un graphe ayant les mêmes sommets mais dont toutes les arêtes sont inversées.
+ * @tparam T
+ * @tparam P
+ * @param graphe Objet graphe dont on veut produire l'inverse
+ * @return Le graphe inverse
+ */
 template <typename T, typename P>
 Graphe<T, P> construireInverse(const Graphe<T, P>& graphe) {
     Graphe<T, P> inverse(graphe.enumererSommets()) ;
@@ -45,6 +75,14 @@ Graphe<T, P> construireInverse(const Graphe<T, P>& graphe) {
     return inverse ;
 }
 
+/**
+ * Visite en largeur d'un graphe, à partir d'un sommet de départ donné.
+ * @tparam T
+ * @tparam P
+ * @param graphe
+ * @param depart
+ * @return
+ */
 template <typename T, typename P>
 std::unordered_map<T, T> explorer_BFS(const Graphe<T, P>& graphe, const T& depart) {
     std::unordered_map<T, T> predecesseurs ;
@@ -70,6 +108,14 @@ std::unordered_map<T, T> explorer_BFS(const Graphe<T, P>& graphe, const T& depar
     return predecesseurs ;
 }
 
+/**
+ * Tri topologique d'un graphe avec l'algorithme de retrait des sommets des notes de cours.
+ * @tparam T
+ * @tparam P
+ * @param graphe Objet graphe dont on veut produire un tri topologique
+ * @return Une liste ordonnée des sommets en ordre de précédence.  Remarque: un tri topologique n'est pas nécessairement
+ * unique.
+ */
 template <typename T, typename P>
 std::vector<T> tri_topologique(Graphe<T, P> graphe) {
     std::vector<T> tri ;
